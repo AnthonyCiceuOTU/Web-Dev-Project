5389a359d91bd5c572fc14d58e808e621e7e0936
JcoMonkey <jerico.robles@ontariotechu.net>
2025-04-10 12:34:08 -0400

    added gitignore

--- Web Dev Group Project/statsVue.js
+++ Web Dev Group Project/statsVue.js
@@ -7,22 +7,22 @@ const app = Vue.createApp({
 }).mount('#app')
 
 function updateStatsVue(result) {
-fetch('http://localhost:3000/api/stats', {
-    method: 'POST',
-    headers: { 'Content-Type': 'application/json' },
-    body: JSON.stringify({ difficulty, result })
-})
-.then(res => res.json())
-.then(data => {
-    //console.log('Full response from server:', data);
-    renderStats(data.stats);
-});
+    fetch('http://localhost:3000/api/stats', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ difficulty, result })
+    })
+    .then(res => res.json())
+    .then(data => {
+        //console.log('Full response from server:', data);
+        renderStats(data.stats);
+    });
 }
 
 function renderStatsVue(statsObj) {
-if (!statsObj) return;
+    if (!statsObj) return;
 
-$(`#${difficulty.toLowerCase()}-wins`).text(statsObj.wins);
-$(`#${difficulty.toLowerCase()}-losses`).text(statsObj.losses);
-$(`#${difficulty.toLowerCase()}-winstreak`).text(statsObj.winstreak);
+    $(`#${difficulty.toLowerCase()}-wins`).text(statsObj.wins);
+    $(`#${difficulty.toLowerCase()}-losses`).text(statsObj.losses);
+    $(`#${difficulty.toLowerCase()}-winstreak`).text(statsObj.winstreak);
 }
